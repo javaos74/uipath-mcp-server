9658f388266f726381fce3897917a304d779c824
Hyungsoo Kim <javaos74@gmail.com>
2025-11-01 23:41:34 +0900

    build: Rebuild frontend with i18n support
    - Fix corrupted main.tsx file
    - Rebuild frontend with all i18n translations
    - Update static assets with new bundle including i18next
    - Update package.json with i18next dependencies
    
    Ready for testing with 4 language support (EN, KO, ZH, JA)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tantml:function_calls>
-<invoke name="strReplace">
-<parameter name="oldStr">import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
-import './index.css'act from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
